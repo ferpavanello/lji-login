7255bb68c4df5db06b75347eb10d4db44a872b3b
Fernando Pavanello <fernando.pavanello2@gmail.com>
2021-07-17 21:12:05 -0300

    Create sign up component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import LoginImage from './components/login-image/LoginImage'
 import LoginForm from './components/login-form/LoginForm'
+import SignUp from './components/sign-up/SignUp'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <div className="form-wrapper">
         <h1 className="form-header">Login to continue</h1>
         <LoginForm />
+        <SignUp />
       </div>
     </div>
   );
---
+++ src/components/sign-up/SignUp.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './SignUp.css'
+import facebookLogo from './../../facebook-logo.png'
+import twitterLogo from './../../twitter-logo.png'
+import { Link } from '@material-ui/core'
+
+export default function SignUp () {
+  return (
+    <div className="signup-wrapper">
+      <label>don't have account yet?</label>
+      <p>
+        <Link href="#" className="register" onClick={event => event.preventDefault()}>
+          Register
+        </Link>
+      </p>
+      <label>or sign up using</label>
+      <div className="group-images">
+        <img className="logo" src={facebookLogo} alt="facebook logo" />
+        <img className="logo" src={twitterLogo} alt="twitter logo" />
+      </div>
+    </div>
+  )
+}
