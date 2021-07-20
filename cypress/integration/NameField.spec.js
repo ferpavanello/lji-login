44d79e61f6304a2d9398a1538ca7c8f72628f5b7
Fernando Pavanello <fernando.pavanello2@gmail.com>
2021-07-20 05:40:23 -0300

    Fix cypress execution problems

---
+++ cypress/integration/NameField.spec.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { mount } from '@cypress/react'
+import NameField from './../../src/components/name-field/NameField'
+
+describe('NameField', () => {
+  it('Shows information', () => {
+    mount(<NameField collectData={() => {}} />)
+    // now use standard Cypress commands
+    const testContent = 'Usuário de teste'
+    cy.get('#name')
+      .type(testContent)
+      .should('have.value', testContent)
+  })
+
+  it('Executes collecData each time when conent is changed', () => {
+    let count = 0
+    mount(<NameField collectData={() => count++} />)
+    // now use standard Cypress commands
+    const testContent = 'Testando'
+    cy.get('#name')
+      .type(testContent)
+      .then(() => {
+        expect(count).to.be.eq(testContent.length)
+      })
+  })
+})
